6b1db369c0e96ee9467889bd589a533ae6b7f040
Raquel <100868145+raquelm16@users.noreply.github.com>
2022-09-13 12:51:05 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let listou = []
+let calcular = document.querySelector('#calcular')
+calcular.addEventListener('click', Calculando)
+let limpar = document.querySelector('#limpar')
+limpar.addEventListener('click', Limpando)
+let lista = document.getElementById('lista').value
+
+function Calculando(){
+    let d = Number.parseFloat(document.getElementById('deslocamento').value)
+    let t = Number.parseFloat(document.getElementById('tempo').value)
+    let res = ("A Velocidade Média é: " + (d / t))
+    document.getElementById('resposta').value = res
+
+    
+}
+
+function Limpando(){
+    d = (" ")
+    t = (" ")
+    res = (" ")
+  document.getElementById('deslocamento').value = d
+  document.getElementById('tempo').value = t
+  document.getElementById('resposta').value = res
+}
+
+for(let i=0; i<3; i++){
+    lista = documento.getElementById('lista').value
+    res = document.getElementById('resposta').value
+    listou.push([res])
+    lista.innerHtml = (listou[i])
+}
